6a6211e7f42c17e8d07927f343e7057f78fe3314
julio <cesarj549@yahoo.com.mx>
2019-12-09 21:34:59 -0600

    agregando datasource

--- src/main/java/com/jcah/models/Usuaios.java
+++ src/main/java/com/jcah/models/Usuaios.java
@@ -1,5 +1,110 @@
 package com.jcah.models;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+
 public class Usuaios {
+
+	private Integer id;
+	private String codigo;
+	private String nombre;
+	private String apellido_paterno;
+	private String apellido_materno;
+	private String correo;
+	private String foto;
+	private Date fecha_nacimiento;
+	private Date fecha_ingreso;
+	private String telefono;
+	private String extension;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido_paterno() {
+		return apellido_paterno;
+	}
+
+	public void setApellido_paterno(String apellido_paterno) {
+		this.apellido_paterno = apellido_paterno;
+	}
+
+	public String getApellido_materno() {
+		return apellido_materno;
+	}
+
+	public void setApellido_materno(String apellido_materno) {
+		this.apellido_materno = apellido_materno;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Date getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(Date fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+	public Date getFecha_ingreso() {
+		return fecha_ingreso;
+	}
+
+	public void setFecha_ingreso(Date fecha_ingreso) {
+		this.fecha_ingreso = fecha_ingreso;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
 
 }
